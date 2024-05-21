8049fdbe65f1bb77a6a75f76e7f6b0e176a94c45
Anastasiya <165375443+anastasiya-red@users.noreply.github.com>
2024-05-21 20:14:44 +0300

    Add files via upload

---
+++ nextiter.py
@@ -0,0 +1,26 @@
+class EvenNumbers:
+
+    def __init__(self, start=0, end=1):
+        self.start = start
+        self.end = end
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.start <= self.end and self.start % 2 == 0:
+            start = self.start
+            self.start += 2
+            return start
+        else:
+            if self.start <= self.end and self.start % 2 != 0:
+                start = self.start + 1
+                self.start += 2
+                return start
+
+        raise StopIteration
+
+
+en = EvenNumbers(10, 25)
+for i in en:
+    print(i)
